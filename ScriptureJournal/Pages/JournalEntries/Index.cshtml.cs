e7f821a70b721b48fb99326d9cca9b4eee0f2acc
Hannah <rog19008@byui.edu>
2020-10-27 14:58:24 -0600

    starting sorting, adding images

--- ScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
+++ ScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ScriptureJournal.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 
 namespace ScriptureJournal.Pages.JournalEntries
 {
@@ -27,8 +28,43 @@ namespace ScriptureJournal.Pages.JournalEntries
         [BindProperty(SupportsGet = true)]
         public string EntryBook { get; set; }
 
+        //sorting
+        public IList<JournalEntry> JournalEntries { get; set; }
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string sortOrder { get; set; }
+
+       
+
         public async Task OnGetAsync()
+            
         {
+            //sorting
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<JournalEntry> journalEntries = from j in _context.JournalEntry
+                                                      select j;
+
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    journalEntries = journalEntries.OrderByDescending(j => j.Book);
+                    break;
+                case "Date":
+                    journalEntries = journalEntries.OrderBy(j => j.EntryDate);
+                    break;
+                case "date_desc":
+                    journalEntries = journalEntries.OrderByDescending(j => j.EntryDate);
+                    break;
+                default:
+                    journalEntries = journalEntries.OrderBy(j => j.Book);
+                    break;
+            }
+
             // Use LINQ to get list of books.
             IQueryable<string> bookQuery = from b in _context.JournalEntry
                                            orderby b.Book
@@ -45,6 +81,7 @@ namespace ScriptureJournal.Pages.JournalEntries
             {
                 entries = entries.Where(x => x.Book == EntryBook);
             }
+            JournalEntries = await journalEntries.AsNoTracking().ToListAsync();
             Books = new SelectList(await bookQuery.Distinct().ToListAsync());
             JournalEntry = await entries.ToListAsync();
         }
